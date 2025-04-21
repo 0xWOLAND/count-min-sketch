99c8557d5957cbcebc2842bd9f098d56279d6f73
Bhargav Annem <bannem@caltech.edu>
2025-04-21 03:35:26 -0700

    chore: paths

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "hashset_zig",
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = .{ .path = "src/root.zig" },
         .target = target,
         .optimize = optimize,
     });
@@ -14,7 +14,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = .{ .path = "src/root.zig" },
         .target = target,
         .optimize = optimize,
     });
